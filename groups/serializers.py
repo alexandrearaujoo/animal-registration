8e796f3188fb83ea0e0ab60d364f0b0eb8715b78
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-14 13:15:01 -0300

    feat: group serializer created

---
+++ groups/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class GroupSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=20)
+    scientific_name = serializers.CharField(max_length=50)
