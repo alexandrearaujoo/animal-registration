afc22a927a5b5ecc74945401b77932e306a2e586
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-14 13:13:27 -0300

    update: relations in animal model

--- animals/models.py
+++ animals/models.py
@@ -1,4 +1,5 @@
 from operator import mod
+import re
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,10 @@ class Animal (models.Model):
     age = models.FloatField()
     weight = models.FloatField()
     sex = models.CharField(max_length=15)
+
+    groups = models.ForeignKey('groups.Group', on_delete=models.CASCADE, related_name='groups')
+    characteriscs = models.ManyToManyField(
+        'characteristcs.Characteristc', 
+        on_delete=models.CASCADE, 
+        related_name='characteristcs'
+    ) 
